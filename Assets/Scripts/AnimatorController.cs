c0cc13b546ef28e064df65f0673ba4d7e6b2a0a0
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-30 16:15:16 -0500

    MadreAnimation

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject madre;
+    [SerializeField]
+    GameObject mano;
+
+    Animator _animator;
+    void Start()
+    {
+        _animator = mano.GetComponent<Animator>();
+        mano.SetActive(false);
+        StartCoroutine(AparecerMano());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator AparecerMano() 
+    {
+        
+        mano.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        _animator.SetBool("Lanzando", true);
+
+    }
+    
+    
+}
