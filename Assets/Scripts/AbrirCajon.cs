68b1771623dd913f3b71660ea1446cdc55c623a9
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-30 09:20:59 -0500

    Audios

--- Assets/Scripts/AbrirCajon.cs
+++ Assets/Scripts/AbrirCajon.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class AbrirCajon : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    List<AudioClip> audios;
+    AudioSource _audioSource;
     void Start()
     {
-        
+        _audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,11 +19,15 @@ public class AbrirCajon : MonoBehaviour
         if (Input.GetMouseButtonDown(1)) 
         {
             gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            _audioSource.clip = audios[1];
+            _audioSource.Play();
         }
     }
     private void OnMouseDown()
     {
         gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        _audioSource.clip= audios[0];
+        _audioSource.Play();
     }
     
 }
