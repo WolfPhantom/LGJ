67e03335352bd64353d52da9798cca643c898c8d
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-30 09:37:48 -0500

    AudioGameOver

---
+++ Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    List<AudioClip> audios;
+    AudioSource _audioSource;
+    
+    void Start()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+        StartCoroutine(EventoChancla());
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator EventoChancla() 
+    {
+        _audioSource.clip = audios[1];
+        _audioSource.Play();
+        yield return new WaitForSeconds(3f);
+        _audioSource.clip = audios[0];
+        _audioSource.Play();
+    }
+}
