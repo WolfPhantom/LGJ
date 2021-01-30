b8a55b3393fd9e375b73e3ecc5c1672671819543
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-29 20:02:55 -0500

    Menu

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    Button playButton;
+
+    [SerializeField]
+    Button quitButton;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(() => GoGameplay());
+        quitButton.onClick.AddListener(() => Quit());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GoGameplay() 
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+     void Quit()
+    {
+        Application.Quit();
+            
+    }
+
+}
