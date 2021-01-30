1ee23bd70b4138e28d0da6694c25776df2ffb29c
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-30 08:40:20 -0500

    ClampInTnTheBox

--- Assets/Scripts/GameoPlay/DragHandler.cs
+++ Assets/Scripts/GameoPlay/DragHandler.cs
@@ -11,6 +11,18 @@ public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginD
 
     Transform startParent;
     Transform dragParent;
+    //Image
+    float halfColliderWidth;
+    float halfColliderHeight;
+    //Background 
+
+    GameObject background;
+    float halfColliderWidthB;
+    float halfColliderHeightB;
+    float minX = 450;
+    float maxX = 1450;
+    float minY = 195;
+    float maxY = 900;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -26,6 +38,7 @@ public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginD
         //Debug.Log("OnDrag");
         Vector3 desirePosition = Input.mousePosition;
         desirePosition.z = 10.0f;
+        desirePosition = CalculateClamped(desirePosition);
         transform.position = Camera.main.ScreenToWorldPoint(desirePosition); ;
     }
 
@@ -61,7 +74,35 @@ public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginD
     // Start is called before the first frame update
     void Start()
     {
+        background = GameObject.FindGameObjectWithTag("CajonBackground");
         dragParent = GameObject.FindGameObjectWithTag("DragParent").transform;
+
+
+        halfColliderWidth = GetComponent<RectTransform>().rect.width / 2;
+        halfColliderHeight = GetComponent<RectTransform>().rect.height / 2;
+
+        halfColliderWidthB = background.GetComponent<RectTransform>().rect.width / 2;
+        halfColliderHeightB = background.GetComponent<RectTransform>().rect.height / 2;
+
+
+
+        //minX = background.GetComponent<RectTransform>().localPosition.x - halfColliderWidthB;
+        //maxX = background.GetComponent<RectTransform>().localPosition.x + halfColliderWidthB;
+
+        //minY = background.GetComponent<RectTransform>().localPosition.y - halfColliderHeightB;
+        //maxY = background.GetComponent<RectTransform>().localPosition.y + halfColliderHeightB;
+
+        //print("halfColliderWidth: " + halfColliderWidth);
+        //print("halfColliderHeight: " + halfColliderHeight);
+        //print("halfColliderWidthB: " + halfColliderWidthB);
+        //print("halfColliderHeightB: " + halfColliderHeightB);
+        //print("minX: " + minX);
+        //print("maxX: " + maxX);
+        //print("minY: " + minY);
+        //print("maxY: " + maxY);
+
+        
+        
         timerController = GameObject.FindGameObjectWithTag("GameController").GetComponent<TimerController>();
     }
 
@@ -69,5 +110,27 @@ public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginD
     void Update()
     {
         
+    }
+    Vector3 CalculateClamped(Vector3 position)
+    {
+        //print(position);
+        // clamp left and right edges
+        if (position.x - halfColliderWidth < minX)
+        {
+            position.x = minX + halfColliderWidth;
+        }
+        else if (position.x + halfColliderWidth > maxX)
+        {
+            position.x = maxX - halfColliderWidth;
+        }
+        if (position.y - halfColliderHeight < minY)
+        {
+            position.y = minY + halfColliderHeight;
+        }
+        else if (position.y + halfColliderHeight > maxY)
+        {
+            position.y = maxY - halfColliderHeight;
+        }
+        return position;
     }
 }
