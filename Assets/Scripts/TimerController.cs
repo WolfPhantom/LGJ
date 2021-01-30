24f4b9713c28c17962c69136afcb2b6e2a703810
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-29 21:05:03 -0500

    Timer

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimerController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject timerBar;
+    public GameObject Timetxt;
+    Timer timer;
+    
+    bool event1=false;
+
+    void Start()
+    {
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = 30;
+        timer.Run();
+        Timetxt.SetActive(false);
+
+    }
+
+    
+    void Update()
+    {
+        if (!timer.Finished)
+        {
+            print(timer.elapsedSeconds);
+            if (event1==true)
+            {
+                event1 = false;
+                LostTime();
+                
+            }
+            timerBar.GetComponent<Image>().fillAmount = 1 - timer.elapsedSeconds / timer.totalSeconds;
+            //print(timer.elapsedSeconds / timer.totalSeconds);
+            
+        }
+        else
+        {
+            Timetxt.SetActive(true);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+    public void LostTime() 
+    {
+        timer.elapsedSeconds += 5;
+    }
+    public void ActivateEvent() 
+    {
+        event1 = true;
+    }
+    
+}
