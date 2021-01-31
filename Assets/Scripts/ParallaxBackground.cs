21925fdd2356ba71c7b94ca3ffcd6e73f60ab252
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-31 09:15:45 -0500

    MenuChanges

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f;
+    [SerializeField]
+    private float offset;
+    private Vector2 starPos;
+    private float newXpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newXpos = Mathf.Repeat(Time.time * -moveSpeed, offset);
+        transform.position = starPos + Vector2.right * newXpos;
+    }
+}
