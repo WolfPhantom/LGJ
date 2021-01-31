01a76afded17084e9530da31639234bdcc4b4984
Alex Trujillano <43768887+WolfPhantom@users.noreply.github.com>
2021-01-30 20:59:15 -0500

    WinScene

---
+++ Assets/Scripts/WinController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Button btnMenu;
+    void Start()
+    {
+        btnMenu.onClick.AddListener(() => GoMenu());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GoMenu() 
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
